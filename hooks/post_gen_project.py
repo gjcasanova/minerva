3a9acae7a4ac25a04ca79fa4c845d455f0daa9aa
Guillermo Casanova <gjcasanova@outlook.com>
2025-01-15 00:31:47 -0500

    fix(hooks): solve env switching error

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,28 @@
 """Post generation script."""
 
 import subprocess
+import sys
 
 ERROR_COLOR = '\x1b[31m'
 MESSAGE_COLOR = '\x1b[33m'
 SUCCESS_COLOR = '\x1b[32m'
 RESET_COLOR = '\x1b[0m'
+
+PYTHON_BASE_PATH = f'{sys.base_prefix}/bin/python3'
+PYTHON_ENV_PATH = '.venv/bin/python3'
+
+
+def create_vitual_environment():
+    """Create the virtual environment."""
+    print(f'{MESSAGE_COLOR}Activating virtual environment...{RESET_COLOR}')
+
+    try:
+        subprocess.run([f'{PYTHON_BASE_PATH}', '-m', 'venv', '.venv'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'{ERROR_COLOR}Virtual environment creation failed: {e}{RESET_COLOR}')
+        raise e
+    else:
+        print(f'{SUCCESS_COLOR}Virtual environment created{RESET_COLOR}')
 
 
 def install_requirements():
@@ -13,7 +30,8 @@ def install_requirements():
     print(f'{MESSAGE_COLOR}Installing project requirements...{RESET_COLOR}')
 
     try:
-        subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
+        subprocess.run([f'{PYTHON_ENV_PATH}', '-m', 'pip', 'install', '--upgrade', 'pip'], check=True)
+        subprocess.run([f'{PYTHON_ENV_PATH}', '-m', 'pip', 'install', '-r', 'requirements.txt'], check=True)
     except subprocess.CalledProcessError as e:
         print(f'{ERROR_COLOR}Instalation of project requirements failed: {e}{RESET_COLOR}')
     else:
@@ -28,8 +46,7 @@ def init_git():
         subprocess.run(['git', 'init'], check=True)
         subprocess.run(['git', 'add', '-A'], check=True)
         subprocess.run(['pre-commit', 'install'], check=True)
-        subprocess.run(['pre-commit', 'run', '--all-files'], check=True)
-        subprocess.run(['git', 'commit', '-m', 'Initial commit'], check=True)
+        subprocess.run(['git', 'commit', '-m', 'chore(core): init project'], check=True)
     except subprocess.CalledProcessError as e:
         print(f'{ERROR_COLOR}Git repository initialization failed: {e}{RESET_COLOR}')
     else:
@@ -38,6 +55,7 @@ def init_git():
 
 def main():
     """Execute the post generation script."""
+    create_vitual_environment()
     install_requirements()
     init_git()
 
